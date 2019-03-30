2b598e640812e5d5b7ab92d9c2952394a2529ae8
developerdiary <ikhlaquesoft@gmail.com>
2019-03-30 10:31:18 +0530

    NavBar file change

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="navbar navbar-dark bg-dark navbar-expand-lg">
     <div class="container d-flex justify-content-between">
-      <a href="#" class="navbar-brand d-flex align-items-center">
+      <router-link :to="{ name: 'home'}" class="navbar-brand d-flex align-items-center">
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-        <strong><router-link :to="{ name: 'home'}">Movies</router-link></strong>
-      </a>
+        <strong>Movies</strong>
+      </router-link>
       
     </div>
   </div>
@@ -12,7 +12,7 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.router-link-active{
+.router-link-active, a.navbar-brand{
   color:#fff;
 }
 </style>
