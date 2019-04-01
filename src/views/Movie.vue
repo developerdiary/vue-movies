9ffce0855344c07d7bdc764956249dc418d0fb25
developerdiary <ikhlaquesoft@gmail.com>
2019-04-01 15:42:46 +0530

    Change file name

--- src/views/Movie.vue
+++ src/views/Movie.vue
@@ -3,11 +3,13 @@
 
     <div class="container-fluid no-padding">
         <div class="mo-profile">
-            <img align="left" class="mo-image-lg" :src="'https://image.tmdb.org/t/p/w1280'+movie.backdropURL"/>
-            <img align="left" class="mo-image-profile thumbnail" :src="'https://image.tmdb.org/t/p/w500'+movie.posterURL" alt="Profile image example"/>
-            <div class="mo-profile-text">
-                <h1>{{movie.title}}</h1>
-                <p>{{movie.releaseDate | formatDate}}</p>
+            <div class="mo-profile-banner" :style="{ backgroundImage: 'url(\'https://image.tmdb.org/t/p/w1280' + movie.backdropURL + '\')' }"></div>            
+            <div class="container">
+              <img align="left" class="mo-image-profile thumbnail" :src="'https://image.tmdb.org/t/p/w500'+movie.posterURL" alt="Profile image example"/>
+              <div class="mo-profile-text">
+                  <h1>{{movie.title}}</h1>
+                  <p>{{movie.releaseDate | formatDate}}</p>
+              </div>
             </div>
         </div>
     </div>
@@ -45,6 +47,12 @@ export default {
 </script>
 
 <style scoped>
+
+.mo-profile-banner {    
+    background-size: cover;
+    height: 400px;
+}
+
 .mo-profile img.mo-image-lg{
     z-index: 0;
     width: 100%;  
@@ -52,24 +60,38 @@ export default {
 }
 
 .mo-image-profile {
-    margin: -200px 10px 0px 120px;
+    margin: -200px 10px 0px 0px;
     z-index: 9;
-    width: 15%; 
+    width: 20%; 
 }
 
-.plot{
-  padding-top:50px;
-}
+  .plot{
+    padding-top:50px;
+  }
+  
+ .mo-profile-text{
+    padding-top: 10px;
+  }
+
  .no-padding{
    padding-left: 0px;
    padding-right: 0px;
  }
 
 @media (max-width:768px) {
-    
+
+  .mo-profile-banner {    
+      background-size: cover;
+      height: 200px;
+  }
+ 
   .mo-profile-text>h1{
       font-weight: 700;
-      font-size:16px;
+      font-size:16px;    
+  }
+
+  .mo-profile-text p{
+    font-size: 14px;
   }
 
   .mo-image-profile
@@ -77,6 +99,9 @@ export default {
       margin: -45px 10px 0px 25px;
       z-index: 9;
       width: 20%; 
+  }
+  .plot{
+    padding-top:20px;
   }
 }
 </style>
